b3dc4516e9327075dfa1b99465d00e0687ee8f10
Timothy McGowen <mcgowen17@yahoo.com>
2019-08-19 14:56:14 -0700

    Starting the test

--- src/controls/Controls.spec.js
+++ src/controls/Controls.spec.js
@@ -1,1 +1,20 @@
 // Test away!
+import React from 'react';
+import render from 'react-testing-library'
+import renderer from 'react-test-renderer';
+//Import react-test-renderer as a --dev dependency and then import
+
+
+import Controls from './Controls'; //Importing componenet that we are testing
+
+
+describe('<Controls />', () => {
+    it('should match snapshot', () => {
+        const tree = renderer.create(<Controls />).toJSON();
+
+        expect(tree).toMatchSnapshot();
+    });
+    it('defaults to unlocked and open', () => {
+
+    })
+});
